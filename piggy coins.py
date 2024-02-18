d8129a561305294ded7b949dacf83dbd016184b8
mrbaiel <151663535+mrbaiel@users.noreply.github.com>
2024-02-18 12:28:12 +0700

    Add files via upload

---
+++ piggy coins.py
@@ -0,0 +1,16 @@
+class Piggy:
+    def __init__(self, balance = 0, max_size = 500):
+        self.balance = balance
+        self.max_size = max_size
+
+    def add_coin(self, coins):
+        if self.balance + coins > self.max_size:
+            return( f"max size is {self.max_size} it's full")
+        else:
+            balance += coins
+
+    def get_coin(self, coins):
+        self.balance -= coins
+
+    def show_balance(self):
+        return self.balance
